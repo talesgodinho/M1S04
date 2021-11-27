cdcd2f47d76680f9fa9199f513f19d991b33bd5f
Tales Godinho <talesflg@hotmail.com>
2021-11-26 21:04:07 -0300

    Adiciona o exercício 5

--- src/exercicio5/AdivinheNumero.java
+++ src/exercicio5/AdivinheNumero.java
@@ -1,4 +1,28 @@
 package exercicio5;
 
+import java.util.Scanner;
+
 public class AdivinheNumero {
+    public static void main(String[] args) {
+
+        System.out.print("Adivinhe qual número de 1 a 5 eu estou pensando: ");
+
+        int randomNumber = (int) (1 + (Math.random() * 5)); // Gera número aleatório entre 1 e 5.
+        Scanner input = new Scanner(System.in);
+        int inputNumber = Integer.parseInt(input.nextLine());
+
+        while (inputNumber > 5 || inputNumber < 1) {
+            System.out.print("Insira um número entre 1 e 5: ");
+            inputNumber = Integer.parseInt(input.nextLine());
+        }
+
+        if (inputNumber == randomNumber){
+            System.out.println("Você acertou!");
+        }
+        else {
+            System.out.printf("Você errou, o número correto era %d", randomNumber);
+        }
+    }
+
 }
+
