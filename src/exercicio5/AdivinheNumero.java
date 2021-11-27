549b3bf1271891b09fcbd58bb1a0ef067c35e5df
Tales Godinho <talesflg@hotmail.com>
2021-11-26 21:10:26 -0300

    Adiciona descrição do exercício 5

--- src/exercicio5/AdivinheNumero.java
+++ src/exercicio5/AdivinheNumero.java
@@ -1,3 +1,10 @@
+/*Crie um programa em Java que, utilizando a classe Scanner, pergunte ao usuário a seguinte frase:
+“Adivinhe qual número de 1 a 5 eu estou pensando”. Na sequência, o usuário deve inserir um
+número entre 1 e 5, e o seu programa deve gerar aleatoriamente outro número, também de 1 a 5.
+Se o número gerado for o mesmo que o usuário inseriu, o programa deve escrever na tela “Você acertou!”,
+mas se for diferente, o programa deve escrever “Você errou, o número correto era X”, onde X é o número gerado
+aleatoriamente pelo programa.*/
+
 package exercicio5;
 
 import java.util.Scanner;
